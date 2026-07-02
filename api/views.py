8b0ece907dc23a100f92f070b48e624a973faabd
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-07-02 23:42:10 +0900

    Creating the api path

--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # view for books
 class BookView(APIView):
-    def get():
-        pass
+    def get(self, request):
+        return Response({"hello": "django"})
 
     def post():
         pass
+
+book_view = BookView.as_view()
