68d9bf3038ccf25b34b7304abc0e15e733d7758c
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-07-03 22:04:33 +0900

    New Function: Checking is test response is correct

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -2,4 +2,7 @@ from rest_framework.test import APITestCase
 
 
 class BookViewTest(APITestCase):
-    pass
+    def test_response_is_correct(self):
+        response = self.client.get()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"hello": "django"})
