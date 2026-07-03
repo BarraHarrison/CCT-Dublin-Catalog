795b3ebd41b95a05f725aafb2a1e5aa9eaf50430
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-07-03 22:32:35 +0900

    More tests for the BookViewTest class

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -6,4 +6,24 @@ class BookViewTest(APITestCase):
     def test_response_is_correct(self):
         url = reverse('api:books')
         response = self.client.get(url, format='json')
-        assert response.status_code == 404
+        assert response.status_code == 200
+
+    def test_response_is_json(self):
+        url = reverse('api:books')
+        response = self.client.get(url, format='json')
+        assert response['Content-Type'] == 'application/json'
+
+    def test_response_contains_hello_key(self):
+        url = reverse('api:books')
+        response = self.client.get(url, format='json')
+        assert 'hello' in response.data
+
+    def test_response_contains_correct_value(self):
+        url = reverse('api:books')
+        response = self.client.get(url, format='json')
+        assert response.data['hello'] == 'django'
+
+    def test_post_method_not_allowed(self):
+        url = reverse('api:books')
+        response = self.client.post(url, {}, format='json')
+        assert response.status_code == 405
