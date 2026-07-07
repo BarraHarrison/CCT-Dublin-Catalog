6d69496b76f73fd0e7a5f6d1b93874322c0ea44c
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-07-07 11:15:21 +0900

    add Book model migration and post endpoint

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,7 +12,11 @@ class BookView(APIView):
         serializer = BookSerializer(all_books, many=True)
         return Response(serializer.data)
 
-    # def post(self, request):
-    #     pass
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 book_view = BookView.as_view()
