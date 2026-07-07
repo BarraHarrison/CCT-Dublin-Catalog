6a36480c3c7f18f4b7f4cdf3f4ff1cea037449ed
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-07-07 10:18:42 +0900

    models and serializers imports added

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
-# view for books
+
 class BookView(APIView):
     def get(self, request):
-        return Response({"hello": "django"})
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
 
     # def post(self, request):
     #     pass
